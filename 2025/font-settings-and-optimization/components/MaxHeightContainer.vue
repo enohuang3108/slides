f073ac7815337a49128e44810de2e08072e6f293
eno.huang <enohuang3108@gmail.com>
2025-07-27 22:35:51 +0800

    ✨ feat: 新增 MaxHeightContainer 組件以處理動態高度和滾動行為

---
+++ 2025/font-settings-and-optimization/components/MaxHeightContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div ref="container" :style="{ flexGrow: 1, overflowY: 'auto', minHeight: height + 'px', maxHeight: height + 'px' }">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { nextTick, onMounted, ref } from 'vue'
+
+const container = ref(null)
+const height = ref(0)
+
+onMounted(async () => {
+  await nextTick()
+  if (container.value) {
+    height.value = container.value.parentElement.offsetHeight
+  }
+})
+</script>
