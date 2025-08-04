65f4a476fe8483065bb4dcc1c40e5b553552c70a
eno.huang <enohuang3108@gmail.com>
2025-08-04 10:25:15 +0800

    ✨ feat: 更新 package.json 以新增依賴項，調整 eslint.config.ts 和 tsconfig.app.json 配置，並新增 slidev-workspace.yaml

--- slide-index/eslint.config.ts
+++ slide-index/eslint.config.ts
@@ -1,4 +1,3 @@
-import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import pluginOxlint from 'eslint-plugin-oxlint'
@@ -15,7 +14,9 @@ export default defineConfigWithVueTs(
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  {
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
+  },
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
