6fa3f1b86c5450a2c1f4d3aa64498270140f48d1
eno.huang <enohuang3108@gmail.com>
2025-08-28 11:04:47 +0800

    🔧fix: add sildev export timeout argument

--- 2025/font-settings-and-optimization/components/MaxHeightContainer.vue
+++ 2025/font-settings-and-optimization/components/MaxHeightContainer.vue
@@ -1,19 +1,11 @@
 <template>
-  <div ref="container" :style="{ flexGrow: 1, overflowY: 'auto', minHeight: height + 'px', maxHeight: height + 'px' }">
+  <div ref="container" :style="{ height: 'inherit' }">
     <slot />
   </div>
 </template>
 
-<script setup>
-import { nextTick, onMounted, ref } from 'vue'
-
-const container = ref(null)
-const height = ref(0)
-
-onMounted(async () => {
-  await nextTick()
-  if (container.value) {
-    height.value = container.value.parentElement.offsetHeight
-  }
-})
-</script>
+<style scoped>
+:deep(html) {
+  overflow-y: auto;
+}
+</style>
